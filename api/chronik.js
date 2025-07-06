20f64ff20b43f3f05b340a766922a7b541699344
Maik <aiskreme@web.de>
2025-07-06 21:21:16 +0200

    Update chronik.js

--- api/chronik.js
+++ api/chronik.js
@@ -1,8 +1,7 @@
 {\rtf1\ansi\ansicpg1252\cocoartf2822
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;\f1\fnil\fcharset0 AppleColorEmoji;\f2\fnil\fcharset0 .AppleSystemUIFontMonospaced-Regular;
-}
-{\colortbl;\red255\green255\blue255;\red184\green93\blue213;\red155\green162\blue177;}
-{\*\expandedcolortbl;;\cssrgb\c77647\c47059\c86667;\cssrgb\c67059\c69804\c74902;}
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
 \paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
 \pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
 
@@ -10,16 +9,9 @@
 \
 import \{ createClient \} from '@supabase/supabase-js';\
 \
-// 
-\f1 \uc0\u55357 \u56615 
-\f0  Trage hier deine echten Werte ein:\
-\pard\tx560\tx1120\tx1680\tx2240\tx2800\tx3360\tx3920\tx4480\tx5040\tx5600\tx6160\tx6720\pardirnatural\partightenfactor0
-
-\f2\fs26 \cf2 const\cf3  supabaseUrl = process.env.SUPABASE_URL;\
-\cf2 const\cf3  supabaseKey = process.env.SUPABASE_SERVICE_KEY;
-\f0\fs24 \cf0 \
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
-\cf0 \
+const supabaseUrl = process.env.SUPABASE_URL;\
+const supabaseKey = process.env.SUPABASE_SERVICE_KEY;\
+\
 const supabase = createClient(supabaseUrl, supabaseKey);\
 \
 export default async function handler(req, res) \{\
