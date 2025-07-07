669fb033b6368ceb7da2b7fa31844810bdfaa13c
Maik <aiskreme@web.de>
2025-07-07 07:14:44 +0200

    ok

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,5 +1,6 @@
-import { createClient } from '@supabase/supabase-js';
-const supabaseUrl = 'https://siygzsqkkrtrgjzvmpvl.supabase.co'; // ersetze mit deinem Supabase URL
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNpeWd6c3Fra3J0cmdqenZtcHZsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE4MjcyNzgsImV4cCI6MjA2NzQwMzI3OH0.4MiO2uothQafTgsMep3s-tfjVp1X99m-3BceiNLnpVA';             // ersetze mit deinem Public Anon Key
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/dist/module/supabase.js';
+
+const supabaseUrl = 'https://siygzsqkkrtrgjzvmpvl.supabase.co'; // deine Supabase URL
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNpeWd6c3Fra3J0cmdqenZtcHZsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE4MjcyNzgsImV4cCI6MjA2NzQwMzI3OH0.4MiO2uothQafTgsMep3s-tfjVp1X99m-3BceiNLnpVA'; // dein Public Anon Key
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
