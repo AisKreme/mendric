e40f5866ef22912569091ec20398e936271c7bae
Maik <aiskreme@web.de>
2025-07-07 16:08:28 +0200

    ok

--- api/supabaseClient.js
+++ api/supabaseClient.js
@@ -1,7 +1,12 @@
-// supabaseClient.js
+import dotenv from 'dotenv';
+dotenv.config();
+
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseServiceKey = process.env.SUPABASE_SERVICE_KEY;
 
+console.log('SUPABASE_URL:', process.env.SUPABASE_URL);
+console.log('SUPABASE_SERVICE_KEY:', process.env.SUPABASE_SERVICE_KEY);
 export const supabase = createClient(supabaseUrl, supabaseServiceKey);
+
