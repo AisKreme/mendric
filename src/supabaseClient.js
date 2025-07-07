5fc74d48a4a937117c60e37778d4c7dd4a05ffeb
Maik <aiskreme@web.de>
2025-07-07 12:35:24 +0200

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,14 +1,4 @@
-<script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js/dist/umd/supabase.min.js"></script>
-<script>
-  // Supabase Client initialisieren und global verfügbar machen
-  window.supabase = createClient(
-    'https://siygzsqkkrtrgjzvmpvl.supabase.co',
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNpeWd6c3Fra3R0cmdqenZtcHZsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE4MjcyNzgsImV4cCI6MjA2NzQwMzI3OH0.4MiO2uothQafTgsMep3s-tfjVp1X99m-3BceiNLnpVA'
-  );
-supabase.from('chronik_entries').select('*').then(({ data, error }) => {
-      console.log(data, error);
-    });
-</script>
+const supabase = window.supabase;
 
 const supabaseUrl = 'https://siygzsqkkrtrgjzvmpvl.supabase.co'; // ersetze mit deinem Supabase URL
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNpeWd6c3Fra3J0cmdqenZtcHZsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE4MjcyNzgsImV4cCI6MjA2NzQwMzI3OH0.4MiO2uothQafTgsMep3s-tfjVp1X99m-3BceiNLnpVA';             // ersetze mit deinem Public Anon Key
